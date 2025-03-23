822e86850596fbd58125b711bf28edb065b9e406
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2025-03-23 21:03:48 +0900

    カテゴリーを結合取得する処理と対応するテストコードを作成.

--- src/test/resources/db/migration/repository/todocategory/common.sql
+++ src/test/resources/db/migration/repository/todocategory/common.sql
@@ -21,4 +21,5 @@ VALUES
 INSERT INTO `todo`
  (`id`, `user_id`, `title`, `is_check`, `created_at`, `updated_at`)
 VALUES
- (1, 1, 'todo-title-1', 1, '2025-02-17 07:04:10', '2025-05-17 07:04:10');
+ (1, 1, 'todo-title-1', 1, '2025-02-17 07:04:10', '2025-05-17 07:04:10'),
+ (2, 1, 'todo-title-2', 0, '2025-02-17 07:04:10', '2025-05-17 07:04:10');
