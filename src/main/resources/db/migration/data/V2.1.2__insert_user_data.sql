97cfd18477f18896efa54919498a952d1f91bcb5
Ryosuke <116938721+o-zack-0390@users.noreply.github.com>
2025-03-27 14:57:09 +0900

    本番環境の初期データを追加.

---
+++ src/main/resources/db/migration/data/V2.1.2__insert_user_data.sql
@@ -0,0 +1,6 @@
+-- ユーザー
+INSERT INTO `user`
+ (`id`, `authority_id`, `name`, `email`, `password`, `created_at`, `updated_at`)
+VALUES
+ (1, 1, 'ozack-user', 'user-email-1', 'user-password-1', '2025-03-26 00:00:00', '2025-03-26 00:00:00'),
+ (2, 2, 'ozack-admin', 'user-email-2', 'user-password-2', '2025-03-26 00:00:00', '2025-03-26 00:00:00');
